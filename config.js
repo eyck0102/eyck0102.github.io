95589e6a3f17978439a417c7715f22c872c67acd
nosomber <135101494+eyck0102@users.noreply.github.com>
2023-11-11 13:32:19 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 var config = {
     texts: [
-        "送给我",      
-        "心爱的小可爱",  
-        "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
+        "Dar a...",      
+        "mi pequeña monada",  
+        "STEFY",
+        "Hoy es tu cumpleaños",
+        "Es nuestro primer cumpleaños juntos",
+        "Nuestro primer cumpleaños juntos",
+        "El día 11 del mes 11",
+        "Tú me tienes a mí",
+        "Yo también te tengo",
+        "Cree...",
+        "Algo hermoso está a punto de suceder",
         "YAMI~~",
+        "我爱你♥️!!!"
     ],
         imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
-        // "今天是你的生日": "./imgs/birthday.jpg",
+        "STEFY": "./imgs/xiaokeai.png",
     },
-    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "Inauguración",
         play: "concierto",
