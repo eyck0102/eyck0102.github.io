36644754ace4f6ac69566593e18480db91c705c8
nosomber <135101494+eyck0102@users.noreply.github.com>
2023-11-11 12:20:32 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+var config = {
+    texts: [
+        "送给我",      
+        "心爱的小可爱",  
+        "今天是你的生日",
+        "这是我们在一起的",
+        "第三个生日了哦",
+        "去年的生日",
+        "还记得在王婆串串吃的呢",
+        "今年要吃好的喽哦",
+        "要把我家可爱猪猪喂饱饱",
+        "然后抱走",
+        "YAMI~~",
+    ],
+        imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai.png",
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "Inauguración",
+        play: "concierto",
+        bannar_coming: "color",
+        balloons_flying: "Parece que falta algo",
+        cake_fadein: "pastel?",
+        light_candle: "vela?",
+        wish_message: "Feliz cumpleaños!!!",
+        story: "Algunas palabras para TI",
+    }
+};
